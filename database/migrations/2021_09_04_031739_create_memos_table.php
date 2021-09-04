48d9e576f921d29f0624058764e70681b60bfa60
shake <hiroki.yamako@ezweb.ne.jp>
2021-09-04 12:36:07 +0900

    first commit

---
+++ database/migrations/2021_09_04_031739_create_memos_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMemosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('memos', function (Blueprint $table) {
+            // trueで自動追加
+            // unsignedにしないと外部キー制約ができない
+            $table->unsignedBigInteger('id', true);
+            $table->longText('content');
+            $table->unsignedBigInteger('user_id');
+            // 論理削除を定義 deleted_atを自動生成
+            $table->softDeletes();
+            // 生のSQL文
+            $table->timestamp('updated_at')->default(\DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
+            $table->timestamp('created_at')->default(\DB::raw('CURRENT_TIMESTAMP'));
+            // 外部キー制約
+            // id は users テーブルにあるものでないといけない
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('memos');
+    }
+}
