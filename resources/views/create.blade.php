8c1c248d85eaebe9a6ff79848f8173ebaf880699
shake <hiroki.yamako@ezweb.ne.jp>
2021-09-05 16:20:05 +0900

    [add]既存タグの表示

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -9,6 +9,12 @@
         <div class="form-group">
             <textarea class="form-control" name="content" rows="3" placeholder="ここにメモを入力"></textarea>
         </div>
+        @foreach($tags as $tag)
+            <div class="form-check form-check-inline mb-3">
+                <input class="form-check-label" type="checkbox" name="tags[]" id="{{ $tag['id'] }}" value="{{ $tag['id'] }}">
+                <label class="form-check-label" for="{{ $tag['id'] }}">{{ $tag['name'] }}</label>
+            </div>
+        @endforeach
         <input type="text" class="form-control w-50 mb-3" name="new_tag" placeholder="新しいタグを入力">
         <button type="submit" class="btn btn-primary">保存</button>
     </form>
