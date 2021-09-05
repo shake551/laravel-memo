71dd14f272fa678ebcafb2d8b854d98b271bc9b8
shake <hiroki.yamako@ezweb.ne.jp>
2021-09-05 18:13:44 +0900

    [add]タグの絞り込み機能

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,11 +27,26 @@ class AppServiceProvider extends ServiceProvider
     {
         // 全てのメソッドがよばれる前によばれるメソッド
         view()->composer('*', function($view) {
-            $memos = Memo::select('memos.*')
-                ->where('user_id', '=', \Auth::id())
-                ->whereNull('deleted_at')
-                ->orderBy('updated_at', 'DESC')
-                ->get();
+            $query_tag = \Request::query('tag');
+            // クエリパラメーターtagがある場合
+            if (!empty($query_tag)){
+                // タグで絞り込み
+                $memos = Memo::select('memos.*')
+                    ->leftJoin('memo_tags', 'memo_tags.memo_id', '=', 'memos.id')
+                    ->where('memo_tags.tag_id', '=', $query_tag)
+                    ->where('user_id', '=', \Auth::id())
+                    ->whereNull('deleted_at')
+                    ->orderBy('updated_at', 'DESC')
+                    ->get();
+            }
+            else{
+                // タグがなければ全て取得
+                $memos = Memo::select('memos.*')
+                    ->where('user_id', '=', \Auth::id())
+                    ->whereNull('deleted_at')
+                    ->orderBy('updated_at', 'DESC')
+                    ->get();
+            }
 
             $tags = Tag::where('user_id', '=', \Auth::id())
                 ->whereNull('deleted_at')
