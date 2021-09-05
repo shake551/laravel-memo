9160a99610d60908ef8a83659864b1925906e260
shake <hiroki.yamako@ezweb.ne.jp>
2021-09-05 16:48:50 +0900

    [add]編集画面でのタグの表示

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -6,15 +6,22 @@
         メモ編集
         <form class="card-body" action="{{ route('destroy') }}" method="post">
             @csrf
-            <input type="hidden", name="memo_id" value="{{ $edit_memo['id'] }}">
+            <input type="hidden", name="memo_id" value="{{ $edit_memo[0]['id'] }}">
             <button type="submit" class="btn btn-primary">削除</button>
         </form>
     </div>
     <form class="card-body" action="{{ route('update') }}" method="POST">
         @csrf
-        <input type="hidden" name="memo_id" value="{{ $edit_memo['id'] }}">
+        <input type="hidden" name="memo_id" value="{{ $edit_memo[0]['id'] }}">
         <div class="form-group">
-            <textarea class="form-control" name="content" rows="3">{{ $edit_memo['content'] }}</textarea>
+            <textarea class="form-control" name="content" rows="3">{{ $edit_memo[0]['content'] }}</textarea>
+            @foreach($tags as $tag)
+                <div class="form-check form-check-inline mb-3">
+                    <input class="form-check-label" type="checkbox" name="tags[]" id="{{ $tag['id'] }}"
+                    value="{{ $tag['id'] }}" {{ in_array($tag['id'], $include_tags) ? 'checked' : ''}}>
+                    <label class="form-check-label" for="{{ $tag['id'] }}">{{ $tag['name'] }}</label>
+                </div>
+            @endforeach
         </div>
         <button type="submit" class="btn btn-primary">更新</button>
     </form>
